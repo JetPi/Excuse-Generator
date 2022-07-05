480032311793e9817d44d5af0811b875a87ced86
Ibrahim Zarraga <ibrahimzarragaam@gmail.com>
2022-07-05 18:52:32 -0400

    Correction applied, is this correct?

--- excuse.js
+++ excuse.js
@@ -2,14 +2,13 @@ let who = ['the dog','my granma','his turtle','my bird', 'a car'];
 let what = ['eat','pissed','crushed','broked', 'tore'];
 let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
 
-const sentenceSelector = () =>{
-    let num = Math.floor(Math.random()*(5-1)+1)
+const sentenceSelector = (subject) =>{
+    let num = Math.floor(Math.random()*(subject.length-1)+1)
     return num;
 }
 
 const sentenceGenerator = (who,what,when) =>{
-
-    let sentence = `${who[sentenceSelector()]} ${what[sentenceSelector()]} my homework ${when[sentenceSelector()]}`
+    let sentence = `${who[sentenceSelector(who)]} ${what[sentenceSelector(what)]} my homework ${when[sentenceSelector(when)]}`
     let result = sentence.charAt(0).toUpperCase() +  sentence.slice(1)
     
     return result;
